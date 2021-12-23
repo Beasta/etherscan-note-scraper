c793cc3b64ea4df925b8be8312dc6a7662b0595c
beasta <@>
2021-12-22 19:52:16 -0700

    implement linting changes on scrape.js

--- scrape.js
+++ scrape.js
@@ -4,8 +4,8 @@
 
 const getNotes = (document) => {
   const figure = document.getElementById('SVGdataReport1');
-  if (! figure) {
-    throw new Error('Could not find SVGdataReport1')
+  if (!figure) {
+    throw new Error('Could not find SVGdataReport1');
   }
 
   const table = figure.firstElementChild;
@@ -17,7 +17,7 @@ const getNotes = (document) => {
 
   const list = [];
 
-  body.childNodes.forEach(element => {
+  body.childNodes.forEach((element) => {
     if (element.nodeName !== 'TR') {
       return;
     }
@@ -26,7 +26,9 @@ const getNotes = (document) => {
     const address = element.childNodes[3].childNodes[0].textContent.trim();
     const note = element.childNodes[3].childNodes[1].textContent.trim();
     const dateCreated = element.childNodes[4].textContent;
-    list.push({ address, nameTag, note, dateCreated });
+    list.push({
+      address, nameTag, note, dateCreated,
+    });
   });
 
   return list;
@@ -35,22 +37,19 @@ const getNotes = (document) => {
 const getAllNotes = async () => {
   let allNotes = [];
   let i = 1;
-
-  while (true) {
+  let notes = ['', ''];
+  while (notes.length !== 0) {
     const url = `https://etherscan.io/mynotes_address?p=${i}`;
     const response = await fetch(url);
     const result = await response.text();
     const doc = new window.DOMParser().parseFromString(result, 'text/html');
-    const notes = getNotes(doc);
+    notes = getNotes(doc);
     allNotes = allNotes.concat(notes);
 
-    if (notes.length === 0) {
-      break;
-    }
-    i++;
+    i += 1;
   }
 
   return allNotes;
 };
 
-getAllNotes().then(console.log)
+getAllNotes().then();
